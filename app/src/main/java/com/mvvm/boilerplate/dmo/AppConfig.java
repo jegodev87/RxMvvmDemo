e1218c7a2fdf1f1757a9a3dfe8175a58782847bb
jegodev <jeev.chn@gmail.com>
2020-09-30 00:04:56 +0530

    Employee repository added

---
+++ app/src/main/java/com/mvvm/boilerplate/dmo/AppConfig.java
@@ -0,0 +1,60 @@
+package com.mvvm.boilerplate.dmo;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.Application;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class AppConfig extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        setOrientation();
+    }
+
+    private void setOrientation() {
+        registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
+            @SuppressLint("SourceLockedOrientationActivity")
+            @Override
+            public void onActivityCreated(@NonNull Activity activity, @Nullable Bundle bundle) {
+
+                activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+            }
+
+            @Override
+            public void onActivityStarted(@NonNull Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityResumed(@NonNull Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityPaused(@NonNull Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityStopped(@NonNull Activity activity) {
+
+            }
+
+            @Override
+            public void onActivitySaveInstanceState(@NonNull Activity activity, @NonNull Bundle bundle) {
+
+            }
+
+            @Override
+            public void onActivityDestroyed(@NonNull Activity activity) {
+
+            }
+        });
+    }
+}
